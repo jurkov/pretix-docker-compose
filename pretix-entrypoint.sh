6fa26644ac5d70651b0ed83946512f42c2dd4f54
Christoph <christoph@jurko.de>
2019-01-29 19:50:00 +0100

    Update pretix-entrypoint.sh

--- pretix-entrypoint.sh
+++ pretix-entrypoint.sh
@@ -7,6 +7,6 @@ sleep 15s
 python3 manage.py compilemessages
 python3 manage.py compilejsi18n
 python3 manage.py collectstatic --noinput
-python3 manage.py compress
+#python3 manage.py compress
 python3 -m pretix migrate --noinput
 uwsgi --ini /etc/pretix/pretix.cfg
